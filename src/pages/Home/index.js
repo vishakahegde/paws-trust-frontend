dd0d97f9708108156bbe593c559a8d338e57d59f
vishakahegde <hegde.vishaka@gmail.com>
2020-06-25 15:29:01 +0200

    Create Home Page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Container, Row, Image, Col } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <div>
+      <Container>
+        <h1>Paws Trust Adoption Centre</h1>
+
+        <Row>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://i.pinimg.com/originals/1e/d3/60/1ed360e382a2a3ff4b217d02a717fdbe.jpg"
+              thumbnail
+            />
+          </Col>
+          <Col>
+            <p>
+              Here in Paws Trust we believe that all dogs deserve a permanent,
+              loving home of their own.
+            </p>
+            <p>
+              We process online applications for adoption Monday – Friday.We
+              welcome volunteer dog walkers and groomers at our centre on
+              weekdays
+            </p>
+            <p>
+              We are a non-profit organisation. We rescue dogs and wish to do
+              more with the support of generous donations, fosters, and
+              volunteers.
+            </p>
+            <p>
+              Our team of volunteers and animal-care attendants are dedicated to
+              providing a high quality of care and meeting each animals’ unique
+              needs through training and enrichment programs.
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_1280.jpg"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://cdn.pixabay.com/photo/2016/11/29/01/24/adorable-1866531_1280.jpg"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://cdn.pixabay.com/photo/2015/02/21/10/39/dog-644111_1280.jpg"
+              thumbnail
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
